8715110ed291145bbff6f7c63685f70987c01d1a
Devin McCurdy <41960449+devin-mccurdy@users.noreply.github.com>
2024-10-28 17:33:23 -0400

    mobile module test reorganization

--- modules/vaos/spec/requests/vaos/v2/locations/clinics_spec.rb
+++ modules/vaos/spec/requests/vaos/v2/locations/clinics_spec.rb
@@ -107,7 +107,7 @@ RSpec.describe 'VAOS::V2::Locations::Clinics', type: :request do
         it 'returns a 404 http status' do
           expect_any_instance_of(VAOS::V2::AppointmentsService)
             .to receive(:get_most_recent_visited_clinic_appointment)
-                  .and_return(nil)
+            .and_return(nil)
           get '/vaos/v2/locations/last_visited_clinic', headers: inflection_header
           expect(response).to have_http_status(:not_found)
         end
@@ -161,7 +161,7 @@ RSpec.describe 'VAOS::V2::Locations::Clinics', type: :request do
         it 'returns a 404 http status' do
           expect_any_instance_of(VAOS::V2::AppointmentsService)
             .to receive(:get_recent_sorted_clinic_appointments)
-                  .and_return(nil)
+            .and_return(nil)
           get '/vaos/v2/locations/recent_clinics', headers: inflection_header
           expect(response).to have_http_status(:not_found)
         end
